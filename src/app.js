d7c685ded54bd0303b71b2b9fdaded9cdb36093c
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2023-12-30 13:03:09 +0200

    added search functionality using APIs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "5e64tc31ab6af99145eb23afo3d1f8c0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(updateTemperature);
+}
 function handleSearchInput(event) {
   event.preventDefault();
   let searchElement = document.querySelector("#city-name-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchElement.value;
+  searchCity(searchElement.value);
 }
 let searchInput = document.querySelector("#search-form");
 searchInput.addEventListener("submit", handleSearchInput);
+searchCity("Durban");
